3db8f63ce89370455e9b910bf19948217b9f7143
Sarathlal <hello@sarathlal.com>
2024-11-08 20:32:28 +0530

    fatal error due to conditional check in wp_head hook resolved

--- includes/class-vnw-public.php
+++ includes/class-vnw-public.php
@@ -9,7 +9,6 @@ class VNW_Public {
     public function __construct() {
         add_action('woocommerce_after_variations_table', [$this, 'display_variation_radio_buttons']);
         add_action('wp_footer', [$this, 'add_variation_radio_buttons_script']);
-        add_action('wp_head', [$this, 'add_custom_css']);
         add_filter('woocommerce_available_variation', [$this, 'add_custom_title_to_variations'], 10, 3);
     }
 
@@ -17,11 +16,12 @@ class VNW_Public {
      * Helper function to check if it's a variable product and if "Display variation name" is enabled.
      */
     private function should_display_variation_names() {
+        global $product;
+        
         if (!is_product()) {
             return false; // Not a product page
         }
 
-        global $product;
         if (!$product || !$product->is_type('variable')) {
             return false; // Not a variable product
         }
@@ -34,11 +34,16 @@ class VNW_Public {
     public function display_variation_radio_buttons() {
         if (!$this->should_display_variation_names()) {
             return; // Exit if the conditions are not met
-        }
+        }     
 
         global $product;
         $available_variations = $product->get_available_variations();
 
+        echo '<style>
+                    .variations_form .variations { 
+                        display: none !important; 
+                    }
+                </style>';
         echo '<div class="variation-options">';
         foreach ($available_variations as $variation_data) {
             $variation = wc_get_product($variation_data['variation_id']); // Get the variation object
@@ -93,20 +98,6 @@ class VNW_Public {
         </script>
         <?php
     }
-
-    // Add custom CSS to hide the default variation dropdowns
-    public function add_custom_css() {
-        if (!$this->should_display_variation_names()) {
-            return; // Exit if the conditions are not met
-        }
-
-        echo '<style>
-            .variations_form .variations { 
-                display: none !important; 
-            }
-        </style>';
-    }
-    
 
     // Add custom title to each variation data for the frontend
     public function add_custom_title_to_variations($variation_data, $product, $variation) {
